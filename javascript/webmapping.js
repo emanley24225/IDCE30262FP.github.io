8a84ed22c2f3716e491141847250e04180dcb2b3
emanley24225 <emanley24225@gmail.com>
2021-11-21 21:53:41 -0500

    javascript file.

---
+++ javascript/webmapping.js
@@ -0,0 +1,5 @@
+var map = L.map('mapid').setView([42.2626, -71.8023], 12);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
