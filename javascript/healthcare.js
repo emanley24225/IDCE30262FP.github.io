6bd00ad4ea504a5405cefe6d2fea0b704e91eff6
jacob-frankel <89650367+jacob-frankel@users.noreply.github.com>
2021-12-05 18:52:46 -0500

    Added captions for popups

--- javascript/healthcare.js
+++ javascript/healthcare.js
@@ -3,7 +3,7 @@ var hc_icon1 = L.icon({
       iconSize: [20,20]
     });
     var hc_marker1 = L.marker([42.273568, -71.748091], {icon: hc_icon1}).addTo(map);{}
-      hc_marker1.bindPopup("<b>St. Annes Free Medical Program</b><br> <p> St. Anne's Free Medical Program is a free healthcare service that aims to help the underserved and the uninsured in the Worcester area. Located in Shrewsbury, they have been operational since 1996. They offer the following services: <ul><li>Work Physicals</li><li>School Physical Exams</li><li>Sick Visits</li><li>Prescription Refills</li><li>Immunizations</li><li>Tuberculosis Testing</li><li>Health Insurance Help and Assistance</li><li>Pediatric Services</li></ul> <br><a href=https://www.worcesterfreecare.org/stAnnes/>Here is their website</a></p>").openPopup();
+      hc_marker1.bindPopup("<b>St. Annes Free Medical Program</b><br> <p> St. Anne's Free Medical Program is a free healthcare service that aims to help the underserved and the uninsured in the Worcester area. Located in Shrewsbury, they have been operational since 1996.").openPopup();
       hc_marker1.on("click", function(e) {
       map.setView(e.latlng, 14);
       hc_marker1.bounce({duration: 500, height: 100}, function(){console.log("done")});
@@ -11,14 +11,14 @@ var hc_icon1 = L.icon({
 
 
     var hc_marker2 = L.marker([42.318278, -71.775047], {icon: hc_icon1}).addTo(map);{}
-      hc_marker2.bindPopup("Greenwood Street Free Medical Program").openPopup();
+      hc_marker2.bindPopup("<b>Greenwood Street Free Medical Program</b><br> <p>The Greenwood Street Free Medical Program seeks to improve the health and well-being of people in need by providing free quality healthcare in an atmosphere of dignity and respect. They serve the uninsured and the underserved in the Worcester and Shrewsbury.").openPopup();
       hc_marker2.on("click", function(e) {
       map.setView(e.latlng, 14);
       hc_marker2.bounce({duration: 500, height: 100}, function(){console.log("done")});
       });
 
     var hc_marker3 = L.marker([42.253673, -71.801801], {icon: hc_icon1}).addTo(map);{}
-      hc_marker3.bindPopup("Central Massachusetts Regional Public Health Alliance").openPopup();
+      hc_marker3.bindPopup("<b>Central Massachusetts Regional Public Health Alliance</b><br><p>The Central MA Regional Public Health Alliance (CMRPHA) is a coalition of five municipalities Towns of Grafton, Millbury, Shrewsbury, West Boylston and the City of Worcester working cooperatively to create and sustain a viable, cost-effective and labor-efficient regional public health district. The regional health district provides a comprehensive array of services to partner municipalities through a single organization managed by Worcester's Division of Public Health.</p>").openPopup();
       hc_marker3.on("click", function(e) {
       map.setView(e.latlng, 14);
       hc_marker3.bounce({duration: 500, height: 100}, function(){console.log("done")});
@@ -32,7 +32,7 @@ var hc_icon1 = L.icon({
       });
 
     var hc_marker5 = L.marker([42.292422, -71.749205], {icon: hc_icon1}).addTo(map);{}
-      hc_marker5.bindPopup("<b>India Society of Worcester Center’s Free Health Stop</b><br> <p> The Free Health Stop is a division of the India Society of Worcester Center as well as the Indian Medical Association of New England, and it is a free healthcare service that aims to help the underserved and the uninsured in the Worcester area. Located in Shrewsbury, they have been operational since 2005. They offer the following services:").openPopup();
+      hc_marker5.bindPopup("<b>India Society of Worcester Center’s Free Health Stop</b><br> <p> The Free Health Stop is a division of the India Society of Worcester Center as well as the Indian Medical Association of New England, and it is a free healthcare service that aims to help the underserved and the uninsured in the Worcester area. Located in Shrewsbury, they have been operational since 2005.").openPopup();
       hc_marker5.on("click", function(e) {
       map.setView(e.latlng, 14);
       hc_marker5.bounce({duration: 500, height: 100}, function(){console.log("done")});
